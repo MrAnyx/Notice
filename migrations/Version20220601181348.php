769eb5dcc45a1a897166559eb5f375173742ad37
Robin <44176707+MrAnyx@users.noreply.github.com>
2022-06-01 18:14:55 +0000

    Fix: Updated migration files #8

--- migrations/Version20220601181348.php
+++ migrations/Version20220601181348.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220530175739 extends AbstractMigration
+final class Version20220601181348 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,9 +20,10 @@ final class Version20220530175739 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE thread (id BINARY(16) NOT NULL COMMENT \'(DC2Type:uuid)\', author_id BINARY(16) DEFAULT NULL COMMENT \'(DC2Type:uuid)\', parent_id BINARY(16) DEFAULT NULL COMMENT \'(DC2Type:uuid)\', content VARCHAR(270) NOT NULL, created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', thread_id BINARY(16) NOT NULL COMMENT \'(DC2Type:uuid)\', UNIQUE INDEX UNIQ_31204C83E2904019 (thread_id), INDEX IDX_31204C83F675F31B (author_id), INDEX IDX_31204C83727ACA70 (parent_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE thread (id BINARY(16) NOT NULL COMMENT \'(DC2Type:uuid)\', author_id BINARY(16) DEFAULT NULL COMMENT \'(DC2Type:uuid)\', parent_id BINARY(16) DEFAULT NULL COMMENT \'(DC2Type:uuid)\', content VARCHAR(270) NOT NULL, created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', thread_id BINARY(16) NOT NULL COMMENT \'(DC2Type:uuid)\', restricted TINYINT(1) DEFAULT 0 NOT NULL, INDEX IDX_31204C83F675F31B (author_id), INDEX IDX_31204C83727ACA70 (parent_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE thread ADD CONSTRAINT FK_31204C83F675F31B FOREIGN KEY (author_id) REFERENCES user (id)');
         $this->addSql('ALTER TABLE thread ADD CONSTRAINT FK_31204C83727ACA70 FOREIGN KEY (parent_id) REFERENCES thread (id)');
+        $this->addSql('ALTER TABLE user CHANGE password password VARCHAR(60) NOT NULL');
     }
 
     public function down(Schema $schema): void
@@ -30,5 +31,6 @@ final class Version20220530175739 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE thread DROP FOREIGN KEY FK_31204C83727ACA70');
         $this->addSql('DROP TABLE thread');
+        $this->addSql('ALTER TABLE user CHANGE password password VARCHAR(255) NOT NULL');
     }
 }
